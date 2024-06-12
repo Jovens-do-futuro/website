ada9a1cc59267fd0190eb2f6106a28fe556212d3
EduLeal <eduardolealaruth@gmail.com>
2024-06-11 21:00:31 -0300

    fix: required nos inputs

--- pages/contato/index.js
+++ pages/contato/index.js
@@ -1,7 +1,10 @@
 //fetch
-var mensagem = document.querySelector("#mensagem")
+var mensagem = document.querySelector("#mensagem");
+var teste123 = document.querySelector("#nome");
+
 document.querySelector("#enviar").addEventListener("click", function teste2() {
   teste(data);
+  console.log(typeof teste123.value)
 })
 
 async function teste(data) {
@@ -16,6 +19,6 @@ async function teste(data) {
 
 const data = {
   "to": "eduardolealaruth@gmail.com",
-  "subject": "Assunto do email",
+  "subject": teste123.value,
   "text": mensagem.value
 }
